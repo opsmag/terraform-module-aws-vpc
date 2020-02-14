48e171e5cf34e10f6e191fd97f135184c9bed2d7
Boris Karaoglanov <bkaraoglanov@astraflare.com>
2020-02-14 02:30:30 -0800

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,360 @@
+terraform {
+  required_version = ">= 0.12.20"
+}
+
+locals {
+  max_subnet_length = "${max(length(var.public_subnets), length(var.private_subnets), length(var.data_subnets))}"
+  nat_gateway_count = "${var.single_nat_gateway ? 1 : (var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length)}"
+
+  vpc_id = "${element(concat(aws_vpc_ipv4_cidr_block_association.this.*.vpc_id, aws_vpc.this.*.id, list("")), 0)}"
+}
+
+# VPC
+resource "aws_vpc" "this" {
+  count = "${var.create_vpc ? 1 : 0}"
+
+  cidr_block                       = "${var.cidr}"
+  instance_tenancy                 = "${var.instance_tenancy}"
+  enable_dns_hostnames             = "${var.enable_dns_hostnames}"
+  enable_dns_support               = "${var.enable_dns_support}"
+  assign_generated_ipv6_cidr_block = "${var.assign_generated_ipv6_cidr_block}"
+
+  tags = "${merge(var.tags, var.vpc_tags, map("Name", format("%s", var.name)))}"
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "this" {
+  count = var.create_vpc && length(var.secondary_cidr_blocks) > 0 ? length(var.secondary_cidr_blocks) : 0
+
+  vpc_id = aws_vpc.this[0].id
+
+  cidr_block = element(var.secondary_cidr_blocks, count.index)
+}
+
+# DHCP Options Set
+resource "aws_vpc_dhcp_options" "this" {
+  count = var.create_vpc && var.enable_dhcp_options ? 1 : 0
+
+  domain_name          = var.dhcp_options_domain_name
+  domain_name_servers  = var.dhcp_options_domain_name_servers
+  ntp_servers          = var.dhcp_options_ntp_servers
+  netbios_name_servers = var.dhcp_options_netbios_name_servers
+  netbios_node_type    = var.dhcp_options_netbios_node_type
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags,
+    var.dhcp_options_tags,
+  )
+}
+
+# DHCP Options Set Association
+resource "aws_vpc_dhcp_options_association" "this" {
+  count = var.create_vpc && var.enable_dhcp_options ? 1 : 0
+
+  vpc_id          = local.vpc_id
+  dhcp_options_id = aws_vpc_dhcp_options.this[0].id
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "this" {
+  count = "${var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0}"
+
+  vpc_id = "${local.vpc_id}"
+
+  tags = "${merge(var.tags, map("Name", format("igw.%s", var.name)))}"
+}
+
+# Publiс routes
+resource "aws_route_table" "public" {
+  count = "${var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0}"
+
+  vpc_id = "${local.vpc_id}"
+
+  tags = "${merge(var.tags, var.public_subnet_tags, map("Name", format("public.${var.name}")))}"
+}
+
+resource "aws_route" "public_internet_gateway" {
+  count = var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0
+
+  route_table_id         = aws_route_table.public[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this[0].id
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+# Private routes
+resource "aws_route_table" "private" {
+  count = "${var.create_vpc && local.max_subnet_length > 0 ? local.nat_gateway_count : 0}"
+
+  vpc_id = "${local.vpc_id}"
+
+  tags = "${merge(var.tags, var.public_subnet_tags, map("Name", format("private-${element(var.azs_latters, count.index)}.${var.name}")))}"
+
+  lifecycle {
+
+    ignore_changes = ["propagating_vgws"]
+  }
+}
+
+resource "aws_route_table" "data" {
+  count = "${var.create_vpc && local.max_subnet_length > 0 ? local.nat_gateway_count : 0}"
+
+  vpc_id = "${local.vpc_id}"
+
+  tags = "${merge(var.tags, var.public_subnet_tags, map("Name", format("data-${element(var.azs_latters, count.index)}.${var.name}")))}"
+
+  lifecycle {
+    # When attaching VPN gateways it is common to define aws_vpn_gateway_route_propagation
+    # resources that manipulate the attributes of the routing table (typically for the private subnets)
+    ignore_changes = ["propagating_vgws"]
+  }
+}
+
+# Intra routes
+resource "aws_route_table" "intra" {
+  count = "${var.create_vpc && length(var.intra_subnets) > 0 ? 1 : 0}"
+
+  vpc_id = "${local.vpc_id}"
+
+  tags = "${merge(map("Name", "${var.name}-intra"), var.tags, var.intra_route_table_tags)}"
+}
+
+# Public subnet
+resource "aws_subnet" "public" {
+  count = "${var.create_vpc && length(var.public_subnets) > 0 && (! var.one_nat_gateway_per_az || length(var.public_subnets) >= length(var.azs)) ? length(var.public_subnets) : 0}"
+
+  vpc_id                  = "${local.vpc_id}"
+  cidr_block              = "${element(concat(var.public_subnets, list("")), count.index)}"
+  availability_zone       = "${element(var.azs, count.index)}"
+  map_public_ip_on_launch = "${var.map_public_ip_on_launch}"
+
+  tags = "${merge(var.tags, var.public_subnet_tags, map("Name", format("public-${element(var.azs_latters, count.index)}.${var.name}")))}"
+}
+
+# Private subnet
+resource "aws_subnet" "private" {
+  count = "${var.create_vpc && length(var.private_subnets) > 0 ? length(var.private_subnets) : 0}"
+
+  vpc_id            = "${local.vpc_id}"
+  cidr_block        = "${var.private_subnets[count.index]}"
+  availability_zone = "${element(var.azs, count.index)}"
+
+  tags = "${merge(var.tags, var.private_subnet_tags, map("Name", format("private-${element(var.azs_latters, count.index)}.${var.name}")))}"
+}
+
+# Data subnet
+resource "aws_subnet" "data" {
+  count = "${var.create_vpc && length(var.data_subnets) > 0 ? length(var.data_subnets) : 0}"
+
+  vpc_id            = "${local.vpc_id}"
+  cidr_block        = "${var.data_subnets[count.index]}"
+  availability_zone = "${element(var.azs, count.index)}"
+
+  tags = "${merge(var.tags, var.data_subnet_tags, map("Name", format("data-${element(var.azs_latters, count.index)}.${var.name}")))}"
+}
+
+# intra subnets - private subnet without NAT gateway
+resource "aws_subnet" "intra" {
+  count = "${var.create_vpc && length(var.intra_subnets) > 0 ? length(var.intra_subnets) : 0}"
+
+  vpc_id            = "${local.vpc_id}"
+  cidr_block        = "${var.intra_subnets[count.index]}"
+  availability_zone = "${element(var.azs, count.index)}"
+
+  tags = "${merge(map("Name", format("%s-intra-%s", var.name, element(var.azs, count.index))), var.tags, var.intra_subnet_tags)}"
+}
+
+# NAT Gateway
+locals {
+  nat_gateway_ips = "${split(",", (var.reuse_nat_ips ? join(",", var.external_nat_ip_ids) : join(",", aws_eip.nat.*.id)))}"
+}
+
+resource "aws_eip" "nat" {
+  count = "${var.create_vpc && (var.enable_nat_gateway && ! var.reuse_nat_ips) ? local.nat_gateway_count : 0}"
+
+  vpc = true
+
+  tags = "${merge(map("Name", format("%s-%s", var.name, element(var.azs, (var.single_nat_gateway ? 0 : count.index)))), var.tags, var.nat_eip_tags)}"
+}
+
+resource "aws_nat_gateway" "this" {
+  count = "${var.create_vpc && var.enable_nat_gateway ? local.nat_gateway_count : 0}"
+
+  allocation_id = "${element(local.nat_gateway_ips, (var.single_nat_gateway ? 0 : count.index))}"
+  subnet_id     = "${element(aws_subnet.public.*.id, (var.single_nat_gateway ? 0 : count.index))}"
+
+  tags = "${merge(var.tags, map("Name", format("%s-%s", var.name, element(var.azs, (var.single_nat_gateway ? 0 : count.index)))))}"
+
+  depends_on = ["aws_internet_gateway.this"]
+}
+
+resource "aws_route" "private_nat_gateway" {
+  count = "${var.create_vpc && var.enable_nat_gateway ? local.nat_gateway_count : 0}"
+
+  route_table_id         = "${element(aws_route_table.private.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${element(aws_nat_gateway.this.*.id, count.index)}"
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+# VPC Endpoint for S3
+data "aws_vpc_endpoint_service" "s3" {
+  count = "${var.create_vpc && var.enable_s3_endpoint ? 1 : 0}"
+
+  service = "s3"
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  count = var.create_vpc && var.enable_s3_endpoint ? 1 : 0
+
+  vpc_id       = local.vpc_id
+  service_name = data.aws_vpc_endpoint_service.s3[0].service_name
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private_s3" {
+  count = var.create_vpc && var.enable_s3_endpoint ? local.nat_gateway_count : 0
+
+  vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
+  route_table_id  = element(aws_route_table.private.*.id, count.index)
+}
+
+resource "aws_vpc_endpoint_route_table_association" "data_s3" {
+  count = var.create_vpc && var.enable_s3_endpoint ? local.nat_gateway_count : 0
+
+  vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
+  route_table_id  = element(aws_route_table.data.*.id, count.index)
+}
+
+resource "aws_vpc_endpoint_route_table_association" "intra_s3" {
+  count = "${var.create_vpc && var.enable_s3_endpoint && length(var.intra_subnets) > 0 ? 1 : 0}"
+
+  vpc_endpoint_id = "aws_vpc_endpoint.s3.id"
+  route_table_id  = "${element(aws_route_table.intra.*.id, 0)}"
+}
+
+resource "aws_vpc_endpoint_route_table_association" "public_s3" {
+  count = var.create_vpc && var.enable_s3_endpoint && length(var.public_subnets) > 0 ? 1 : 0
+
+  vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
+  route_table_id  = aws_route_table.public[0].id
+}
+
+# VPC Endpoint for DynamoDB
+data "aws_vpc_endpoint_service" "dynamodb" {
+  count = "${var.create_vpc && var.enable_dynamodb_endpoint ? 1 : 0}"
+
+  service = "dynamodb"
+}
+
+resource "aws_vpc_endpoint" "dynamodb" {
+  count = var.create_vpc && var.enable_dynamodb_endpoint ? 1 : 0
+
+  vpc_id       = local.vpc_id
+  service_name = data.aws_vpc_endpoint_service.dynamodb[0].service_name
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private_dynamodb" {
+  count = var.create_vpc && var.enable_dynamodb_endpoint ? local.nat_gateway_count : 0
+
+  vpc_endpoint_id = aws_vpc_endpoint.dynamodb[0].id
+  route_table_id  = element(aws_route_table.private.*.id, count.index)
+}
+
+resource "aws_vpc_endpoint_route_table_association" "data_dynamodb" {
+  count = var.create_vpc && var.enable_dynamodb_endpoint ? local.nat_gateway_count : 0
+
+  vpc_endpoint_id = aws_vpc_endpoint.dynamodb[0].id
+  route_table_id  = element(aws_route_table.data.*.id, count.index)
+}
+
+resource "aws_vpc_endpoint_route_table_association" "intra_dynamodb" {
+  count = "${var.create_vpc && var.enable_dynamodb_endpoint && length(var.intra_subnets) > 0 ? 1 : 0}"
+
+  vpc_endpoint_id = "aws_vpc_endpoint.dynamodb.id"
+  route_table_id  = "${element(aws_route_table.intra.*.id, 0)}"
+}
+
+resource "aws_vpc_endpoint_route_table_association" "public_dynamodb" {
+  count = var.create_vpc && var.enable_dynamodb_endpoint && length(var.public_subnets) > 0 ? 1 : 0
+
+  vpc_endpoint_id = aws_vpc_endpoint.dynamodb[0].id
+  route_table_id  = aws_route_table.public[0].id
+}
+
+# Route table association
+resource "aws_route_table_association" "private" {
+  count = "${var.create_vpc && length(var.private_subnets) > 0 ? length(var.private_subnets) : 0}"
+
+  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private.*.id, (var.single_nat_gateway ? 0 : count.index))}"
+}
+
+resource "aws_route_table_association" "data" {
+  count = "${var.create_vpc && length(var.data_subnets) > 0 ? length(var.data_subnets) : 0}"
+
+  subnet_id      = "${element(aws_subnet.data.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.data.*.id, (var.single_nat_gateway ? 0 : count.index))}"
+}
+
+resource "aws_route_table_association" "public" {
+  count = var.create_vpc && length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
+
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public[0].id
+}
+
+# VPN Gateway
+resource "aws_vpn_gateway" "this" {
+  count = var.create_vpc && var.enable_vpn_gateway ? 1 : 0
+
+  vpc_id          = local.vpc_id
+  amazon_side_asn = var.amazon_side_asn
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags,
+    var.vpn_gateway_tags,
+  )
+}
+
+resource "aws_vpn_gateway_attachment" "this" {
+  count = "${var.vpn_gateway_id != "" ? 1 : 0}"
+
+  vpc_id         = "${local.vpc_id}"
+  vpn_gateway_id = "${var.vpn_gateway_id}"
+}
+
+resource "aws_vpn_gateway_route_propagation" "public" {
+  count = "${var.create_vpc && var.propagate_public_route_tables_vgw && (var.enable_vpn_gateway || var.vpn_gateway_id != "") ? 1 : 0}"
+
+  route_table_id = "${element(aws_route_table.public.*.id, count.index)}"
+  vpn_gateway_id = "${element(concat(aws_vpn_gateway.this.*.id, aws_vpn_gateway_attachment.this.*.vpn_gateway_id), count.index)}"
+}
+
+resource "aws_vpn_gateway_route_propagation" "private" {
+  count = "${var.create_vpc && var.propagate_private_route_tables_vgw && (var.enable_vpn_gateway || var.vpn_gateway_id != "") ? length(var.private_subnets) : 0}"
+
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+  vpn_gateway_id = "${element(concat(aws_vpn_gateway.this.*.id, aws_vpn_gateway_attachment.this.*.vpn_gateway_id), count.index)}"
+}
+
+# Defaults
+resource "aws_default_vpc" "this" {
+  count = "${var.manage_default_vpc ? 1 : 0}"
+
+  enable_dns_support   = "${var.default_vpc_enable_dns_support}"
+  enable_dns_hostnames = "${var.default_vpc_enable_dns_hostnames}"
+  enable_classiclink   = "${var.default_vpc_enable_classiclink}"
+
+  tags = "${merge(map("Name", format("%s", var.default_vpc_name)), var.tags, var.default_vpc_tags)}"
+}
